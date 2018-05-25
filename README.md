dcb25b0de29d52928d2839f4998ceedbb03494d1
Barend Erasmus <developersworkspace@gmail.com>
2018-05-25 14:06:49 +0200

    update

--- README.md
+++ README.md
@@ -41,28 +41,30 @@ User-Agent: Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)
 HTTP/1.1 200 OK
 Content-Length: 44
 Connection: close
-  
+(blank line)
 <html><body><h1>It works!</h1></body></html>
 ```
 
 ---
 
-# Demo
+# Agenda
 
-* Methods (GET, POST) and Paths
+* Methods and Paths
 * Headers
 * Multiple Domains on 1 Port
-* Serving Files
 * URL Rewrite
+* Serving Files
 
 ---
 
 ## Methods and Paths
 
 ```typescript
-const headerLinePattern: RegExp = new RegExp(/([A-Z]{3,4}) (.*) HTTP\/1\.1/);
+const headerLinePattern: RegExp = 
+    new RegExp(/([A-Z]{3,4}) (.*) HTTP\/1\.1/);
 
-const headerLinematches: RegExpMatchArray = lines[0].match(headerLinePattern);
+const headerLinematches: RegExpMatchArray = 
+    lines[0].match(headerLinePattern);
 
 const method: string = headerLinematches[1];
 const path: string = headerLinematches[2];
@@ -80,7 +82,8 @@ const headers: {} = {};
 const headersPattern: RegExp = new RegExp(/(.*): (.*)/);
 
 for (let index = 1; index < lines.length - 1; index ++) {
-    const headersMatches: RegExpMatchArray = lines[index].match(headersPattern);
+    const headersMatches: RegExpMatchArray = 
+        lines[index].match(headersPattern);
 
     const key: string = headersMatches[1];
     const value: string = headersMatches[2];
@@ -88,20 +91,66 @@ for (let index = 1; index < lines.length - 1; index ++) {
     headers[key] = value;
 }
 
-console.log(headers);
+console.log('Headers:');
+for (const key of Object.keys(headers)) {
+    console.log(`${key}: ${chalk.green(headers[key])}`);
+}
 ```
 ---
 
 ## Multiple Domains on 1 Port
 
----
+```typescript
+const host: string = headers['Host'];
 
-## Serving Files
+switch (domain) {
+    case 'localhost:8080':
+        console
+            .log(`Domain: ${chalk.green('localhost')}`);
+        break;
+    case 'my-app.local:8080':
+        console
+            .log(`Domain: ${chalk.green('my-app.local')}`);
+        break;
+    case 'simple-http-server.local:8080':
+        console
+            .log(`Domain: ${chalk.green('simple-http-server.local')}`);
+        break;
+}
+```
 
 ---
 
 ## URL Rewrite
 
+```typescript
+if (urlPath.startsWith('/home.html')) {
+    urlPath = `/my-other-app/home.html`;
+}
+
+console.log(`Path: ${chalk.green(urlPath)}`);
+```
+
+---
+
+## Serving Files
+
+```typescript
+const filePath: string = path.join(__dirname, 'www', urlPath);
+
+    if (fs.existsSync(filePath)) {
+        const body: string = fs.readFileSync(filePath, 'utf8');
+        sendResponse(
+            body, 'text/html', 'OK', 200, socket);
+    } else {
+        sendResponse(
+            'FILE DOES NOT EXIST',
+            'text/html',
+            'Not Found',
+            404,
+            socket);
+    }
+```
 ---
 
 # Resources
--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,28 @@
 import chalk from 'chalk';
+import * as fs from 'fs';
 import * as net from 'net';
+import * as path from 'path';
 
 export function handle(lines: string[], socket: net.Socket): void {
     // STEP: 1
     // console.log(lines);
 
-    // STEP: 2 - Get Method and Path
+    // STEP: 2 - Methods and Path
     const headerLinePattern: RegExp = new RegExp(/([A-Z]{3,4}) (.*) HTTP\/1\.1/);
 
     const headerLinematches: RegExpMatchArray = lines[0].match(headerLinePattern);
 
     const method: string = headerLinematches[1];
-    const path: string = headerLinematches[2];
+    let urlPath: string = headerLinematches[2];
 
-    // console.log(`Method: ${chalk.green(method)}`);
-    // console.log(`Path: ${chalk.green(path)}`);
+    console.log(`Method: ${chalk.green(method)}`);
+    console.log(`Path: ${chalk.green(urlPath)}`);
 
-    // STEP: 3 - Get Headers
+    // STEP: 3 - Headers
     const headers: {} = {};
     const headersPattern: RegExp = new RegExp(/(.*): (.*)/);
 
-    for (let index = 1; index < lines.length - 1; index ++) {
+    for (let index = 1; index < lines.length - 1; index++) {
         const headersMatches: RegExpMatchArray = lines[index].match(headersPattern);
 
         const key: string = headersMatches[1];
@@ -29,15 +31,50 @@ export function handle(lines: string[], socket: net.Socket): void {
         headers[key] = value;
     }
 
-    // console.log(headers);
+    console.log('Headers:');
+    for (const key of Object.keys(headers)) {
+        console.log(`    ${key}: ${chalk.green(headers[key])}`);
+    }
 
     // STEP: 4 - Multiple Domains on 1 Port
+    const domain: string = headers['Host'];
+
+    switch (domain) {
+        case 'localhost:8080':
+            console.log(`Domain: ${chalk.green('localhost')}`);
+            break;
+        case 'my-app.local:8080':
+            console.log(`Domain: ${chalk.green('my-app.local')}`);
+            break;
+        case 'simple-http-server.local:8080':
+            console.log(`Domain: ${chalk.green('simple-http-server.local')}`);
+            break;
+    }
 
     // Step: 5 - Url Rewrite
+    if (urlPath.startsWith('/home.html')) {
+        urlPath = `/my-other-app/home.html`;
+    }
+
+    console.log(`Path: ${chalk.green(urlPath)}`);
 
     // STEP: 6 - Serving Files
+    const filePath: string = path.join(__dirname, 'www', urlPath);
 
-    // STEP: X
-    const bodyOfResponse: string = 'Hello World';
-    socket.write(`HTTP/1.1 200 OK\r\ncontent-length: ${bodyOfResponse.length}\r\nconnection: close\r\n\r\n${bodyOfResponse}`);
+    if (fs.existsSync(filePath)) {
+        const body: string = fs.readFileSync(filePath, 'utf8');
+        sendResponse(body, 'text/html', 'OK', 200, socket);
+    } else {
+        sendResponse('FILE DOES NOT EXIST', 'text/html', 'Not Found', 404, socket);
+    }
+}
+
+function sendResponse(
+    body: string,
+    contentType: string,
+    reasonPhrase: string,
+    statusCode: number,
+    socket: net.Socket,
+): void {
+    socket.write(`HTTP/1.1 ${statusCode} ${reasonPhrase}\r\ncontent-length: ${body.length}\r\ncontent-type: ${contentType}\r\nconnection: close\r\n\r\n${body}`);
 }
